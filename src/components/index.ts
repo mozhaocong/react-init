e3646f49c39ed4aebbbd0e52416daae67d9c873c
mozhaocong <>
2022-11-02 17:42:06 +0800

    feature：移除组件库的代码

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,14 +1,14 @@
-export { default as CheckBox } from './model/checkBox'
 export {
 	BaseFormColumnsItem,
 	BaseFormListColumnsItem,
 	BaseFormTableColumnsItem,
 	BaseSearchCheckedListSearch,
 	BaseSearchColumnsItem,
-	BaseTableColumns
-} from './model/classMethod'
-export { default as HtForm } from './model/Form'
-export { default as MinMaxInput } from './model/MinMaxInput'
-export { default as Modal } from './model/Modal'
-export { default as SearchTable } from './model/SearchTable'
-export { default as Timeline } from './model/Timeline'
+	BaseTableColumns,
+	CheckBox,
+	HtForm,
+	MinMaxInput,
+	Modal,
+	SearchTable,
+	Timeline
+} from 'react-mzc-components'
